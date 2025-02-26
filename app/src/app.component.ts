0f90bd21a1f499c154ef34d3e6d829f9e8724feb
Gonçalo Alves <goncalo.alves@knowledgeworks.pt>
2025-02-26 11:03:38 +0000

    working example for ng-template with granular content injection.

--- app/src/app.component.ts
+++ app/src/app.component.ts
@@ -1,43 +1,56 @@
-import { Component, OnInit, ViewChild, forwardRef } from '@angular/core';
-import { CalendarOptions, Calendar, EventClickArg } from '@fullcalendar/core';
+import { AfterViewInit, Component, forwardRef, TemplateRef, ViewChild } from '@angular/core';
+import { FullCalendarComponent } from '@fullcalendar/angular';
+import { Calendar, CalendarOptions, EventClickArg } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg, EventDragStopArg } from '@fullcalendar/interaction';
-import { FullCalendarComponent } from '@fullcalendar/angular';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-
+export class AppComponent implements AfterViewInit {
   calendarOptions?: CalendarOptions;
+
   eventsModel: any;
+
   @ViewChild('fullcalendar') fullcalendar?: FullCalendarComponent;
 
-  ngOnInit() {
+  @ViewChild('someCellTemplate') someCellTemplate?: TemplateRef<any>;
+
+  @ViewChild('someHeaderTemplate') someHeaderTemplate?: TemplateRef<any>;
+
+  ngAfterViewInit() {
     // need for load calendar bundle first
     forwardRef(() => Calendar);
 
     this.calendarOptions = {
-      plugins: [dayGridPlugin, interactionPlugin],
+      plugins: [dayGridPlugin, interactionPlugin, resourceTimelinePlugin],
       editable: true,
+      initialView: 'resourceTimelineMonth',
       customButtons: {
         myCustomButton: {
           text: 'custom!',
           click: function () {
             alert('clicked the custom button!');
-          }
-        }
+          },
+        },
       },
       headerToolbar: {
         left: 'prev,next today myCustomButton',
         center: 'title',
-        right: 'dayGridMonth'
+        right: 'dayGridMonth',
       },
       dateClick: this.handleDateClick.bind(this),
       eventClick: this.handleEventClick.bind(this),
-      eventDragStop: this.handleEventDragStop.bind(this)
+      eventDragStop: this.handleEventDragStop.bind(this),
+      resourceAreaColumns: [
+        {
+          cellContent: this.someCellTemplate,
+          headerContent: this.someHeaderTemplate,
+        },
+      ],
     };
   }
 
@@ -57,7 +70,7 @@ export class AppComponent implements OnInit {
     this.calendarOptions!.headerToolbar = {
       left: 'prev,next myCustomButton',
       center: 'title',
-      right: ''
+      right: '',
     };
   }
 
@@ -65,11 +78,15 @@ export class AppComponent implements OnInit {
     const nowDate = new Date();
     const yearMonth = nowDate.getUTCFullYear() + '-' + (nowDate.getUTCMonth() + 1);
 
+    this.calendarOptions!.resources = [{
+      title: 'Updated Event',
+      id: '1',
+    }];
     this.calendarOptions!.events = [{
       title: 'Updated Event',
       start: yearMonth + '-08',
-      end: yearMonth + '-10'
+      end: yearMonth + '-10',
+      resourceId: '1',
     }];
   }
-
 }
